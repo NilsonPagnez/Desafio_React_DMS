818a8041397147967b744858c9e0e5613ca56504
Nilson Pagnez <nilson.pagneto@gmail.com>
2023-03-01 13:13:35 -0300

    1.3

--- src/components/pages/Notas.js
+++ src/components/pages/Notas.js
@@ -6,7 +6,7 @@ function Notas(){
             <h1>Parabéns</h1>
             <h2>Você tirou 10 em todas as matérias</h2>
             <a href="https://pt.lovepik.com/images/png-1145027.html"><img src={comemoracao }/></a>
-            <h3>(Sim, eu fiquei com preguiça de fazer a tabela das notas do semestre)</h3>
+            
         </section>
     )
 }
